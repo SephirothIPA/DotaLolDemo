80d47e54f1710d9e99dfa75bf180bc609b602a8d
SephirothIPA <banana0502@msn.com>
2013-05-27 19:23:21 +0800

    Initial Commit

---
+++ DotaLolDemo/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  DotaLolDemo
+//
+//  Created by 张瑞 on 13-5-27.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ DotaLolDemo/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  DotaLolDemo
+//
+//  Created by 张瑞 on 13-5-27.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
